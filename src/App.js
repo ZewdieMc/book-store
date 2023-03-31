dc20dde5b8c8a6e337d22aded845572033e22fad
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-31 09:09:41 +0300

    Render NavBar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Categories from './components/Categories';
 import Home from './components/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
