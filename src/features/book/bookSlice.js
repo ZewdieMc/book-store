54a3aa05f66f8e11a9beae89db996740ca475ec4
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-03 10:42:07 +0000

    Create bookSlice

---
+++ src/features/book/bookSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    // Add reducers here
+  },
+});
+
+export default bookSlice.reducer;
