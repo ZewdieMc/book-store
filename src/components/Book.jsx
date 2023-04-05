45649e4bad82a895ad9b3ab132b582081a167517
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-05 22:43:41 +0000

    Dispatch remove book actionn to the store from the book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,15 +1,20 @@
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { removeBook } from '../redux/bookSlice';
-const Book = ({ book }) => (
-  <li>
-    <h2>{ book.title }</h2>
-    <p>{ book.author }</p>
-    <div className='book-actions-container'>
-      <button type="button">Remove</button>
-      <button type="button">Edit</button>
-    </div>
-  </li>
-);
+import { removeBook } from '../redux/books/bookSlice';
+
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
+  return (
+    <li>
+      <h2>{ book.title }</h2>
+      <p>{ book.author }</p>
+      <div className="book-actions-container">
+        <button type="button" onClick={() => dispatch(removeBook(book.item_id))}>Remove</button>
+        <button type="button">Edit</button>
+      </div>
+    </li>
+  );
+};
 
 Book.propTypes = {
   book: PropTypes.shape({
