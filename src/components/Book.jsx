64fd2babc0cd3df146134c2f1343039b5721eb85
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-05 20:29:39 +0000

    Add remove event handler to the book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
-
+import { removeBook } from '../redux/bookSlice';
 const Book = ({ book }) => (
   <li>
     <h2>{ book.title }</h2>
     <p>{ book.author }</p>
-    <button type="button">Remove Book</button>
+    <div className='book-actions-container'>
+      <button type="button">Remove</button>
+      <button type="button">Edit</button>
+    </div>
   </li>
 );
 
 Book.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
