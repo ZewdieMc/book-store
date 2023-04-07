87984d218e114d7af718a25f780a623b3b5b4f56
ZewdieMc <zewdiehabtie26@gmail.com>
2023-04-08 00:29:47 +0300

    Import fetchBooks action creator

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,8 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchBooks } from '../redux/books/bookSlice';
 import Book from './Book';
 
 const BookList = () => {
   const books = useSelector((state) => state.books.bookList);
+  const isLoading = useSelector((state) => state.books.isLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <ul>
       {books.map((book) => (
