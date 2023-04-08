dde19f6b0603a67f4d72dd5820f803d820b88f70
ZewdieMc <zewdiehabtie26@gmail.com>
2023-04-08 11:14:58 +0300

    Add missing JSX to Book Component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,26 +1,48 @@
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { removeBook, deleteBook } from '../redux/books/bookSlice';
 
 const Book = ({ book }) => {
+  const { percent, setPercent } = useState(0);
   const dispatch = useDispatch();
+
+  const handleProgress = () => {
+    setPercent(percent + 2);
+  };
+
+  const handleRemoveBook = (book) => {
+    dispatch(deleteBook(book.item_id))
+      .then(() => dispatch(removeBook(book.item_id)));
+  };
+
   return (
     <li>
-      <span>{book.category}</span>
-      <h2>{ book.title }</h2>
-      <p>{ book.author }</p>
-      <div className="book-actions-container">
-        <button type="button">Comments</button>
-        <button
-          type="button"
-          onClick={
-          () => dispatch(deleteBook(book.item_id))
-            .then(() => dispatch(removeBook(book.item_id)))
-          }
-        >
-          Remove
-        </button>
-        <button type="button">Edit</button>
+      <div className="book-detail">
+        <span>{book.category}</span>
+        <h2>{book.title}</h2>
+        <p>{book.author}</p>
+        <div className="book-actions-container">
+          <button type="button">Comments</button>
+          <button type="button" onClick={() => handleRemoveBook(book)}>Remove</button>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+      <div className="book-progress">
+        <CircularProgressbar value={percent} text={`${percent}%`} />
+        <div className="text-peogress">
+          <span className="Percent-Complete">
+            {`${percent}%`}
+
+          </span>
+          <span className="Complete-text">Completed</span>
+        </div>
+      </div>
+      <div className="book-chapter">
+        <span>Current Chapter</span>
+        <h2>Chapter 17</h2>
+        <button onClick={handleProgress} type="button">Update Progress</button>
       </div>
     </li>
   );
