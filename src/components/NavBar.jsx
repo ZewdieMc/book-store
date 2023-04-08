d3557338536a3f95dd45b551ccfe202103418295
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-08 13:23:36 +0000

    change the nav items to uppercase

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 import { Link } from 'react-router-dom';
+import { FaRegUserCircle } from 'react-icons/fa';
 
 const NavBar = () => (
   <nav>
     <h1>Bookstore CMS</h1>
     <ul>
       <li>
-        <Link to="/">Books</Link>
+        <Link to="/">BOOKS</Link>
       </li>
       <li>
-        <Link to="/categories">Categories</Link>
+        <Link to="/categories">CATEGORIES</Link>
       </li>
     </ul>
+    <div className="user-icon"><FaRegUserCircle className="fa-icon" /></div>
   </nav>
 );
 
